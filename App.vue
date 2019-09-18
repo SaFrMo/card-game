90eca6239fa15e40382eb8d0d0535266755c36db
Sander Moolin <sandermoolin@gmail.com>
2019-09-18 12:46:17 -0400

    Shuffle working

--- App.vue
+++ App.vue
@@ -1,22 +1,35 @@
 <template>
-    <main class="app"></main>
+    <main class="app">
+        <ul :key="deck.lastShuffleTimestamp">
+            <li v-for="(card, i) in deck.orderedDrawPileCards" :key="i">
+                {{ card }}
+            </li>
+        </ul>
+        <button @click="deck.shuffleDrawPile()">Shuffle</button>
+    </main>
 </template>
 
 <script>
 import Card from './built/Card'
 import CardDefaults from './built/CardDefaults'
+import Deck from './built/deck'
 const { BASIC_ATTACK, BASIC_MOVE, BASIC_DEFEND } = CardDefaults
 
 export default {
     data() {
         return {
-            inventory: [BASIC_ATTACK, BASIC_DEFEND],
             discardPile: [],
-            drawPile: []
+            deck: new Deck({
+                inventory: [
+                    BASIC_ATTACK,
+                    BASIC_ATTACK,
+                    BASIC_ATTACK,
+                    BASIC_DEFEND,
+                    BASIC_DEFEND,
+                    BASIC_DEFEND
+                ]
+            })
         }
-    },
-    methods: {
-        shuffle() {}
     }
 }
 </script>
--- lib/deck/index.ts
+++ lib/deck/index.ts
@@ -12,26 +12,43 @@ export default class Deck {
     handIndices: Array<number>
     discardPileIndices: Array<number>
 
-    constructor({ inventory = [], temporaries = [] } = {}) {
+    // meta
+    lastShuffleTimestamp: number
+
+    constructor({ inventory = [], temporaries = [], shuffle = true } = {}) {
         this.inventory = inventory
         this.temporaries = temporaries
+
+        if (shuffle) {
+            this.shuffleDrawPile()
+        }
     }
 
     //
-    // === Basic deck function ===
+    // === Basic deck functions ===
     //
     shuffleDrawPile() {
         // reset draw pile to match count
         this.drawPileIndices = []
-        // TODO: start here
-        for (let i = 0; i < this.fullDeck.length; i++) {}
+        for (let i = 0; i < this.fullDeck.length; i++) {
+            this.drawPileIndices.push(i)
+        }
+        this.drawPileIndices = shuffle(this.drawPileIndices)
+        this.lastShuffleTimestamp = Date.now()
     }
-    draw(count: number = 1) {}
+    // draw(count: number = 1) {}
 
     //
     // === Misc accessors ===
     //
     get fullDeck(): Array<Card> {
         return this.inventory.concat(this.temporaries)
+    }
+    get orderedDrawPileCards(): Array<Card> {
+        const output: Array<any> = []
+        for (let i of this.drawPileIndices) {
+            output.push(this.fullDeck[i])
+        }
+        return output
     }
 }
